4f30c7f84bcdc0cede55a45b9ff03b5797c9515c
icelyAdventures <baik.kaishan@gmail.com>
2023-08-25 02:20:53 +0800

    responsive edit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
 import "./css-styles/triangles.css";
-import "./index1.css";
+import "./index.css";
 import App from "./App";
 // import { ParallaxProvider } from "react-scroll-parallax";
 
